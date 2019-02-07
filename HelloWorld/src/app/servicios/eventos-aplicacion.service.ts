a29745f56b83e26027bfe072d32d759d2267931a
Adrian Eguez <eadepto@hotmail.com>
2019-02-07 09:51:20 -0500

    Actualizar libreria

--- HelloWorld/src/app/servicios/eventos-aplicacion.service.ts
+++ HelloWorld/src/app/servicios/eventos-aplicacion.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from "@angular/core";
-import {EventosAplicacionAbstractService} from "~/app/eventos-applicacion/eventos-aplicacion-abstract.service";
-import {MlLoggerService} from "@manticore-labs/nativescript";
+import {MlLoggerService, EventosAplicacionAbstractService} from "@manticore-labs/nativescript";
 
 @Injectable()
 export class EventosAplicacionService extends EventosAplicacionAbstractService {
--- HelloWorld/src/main.ts
+++ HelloWorld/src/main.ts
@@ -3,12 +3,11 @@ import {platformNativeScriptDynamic} from "nativescript-angular/platform";
 import {AppModule} from "./app/app.module";
 import {ios} from "tns-core-modules/application";
 import {EventosDelegate} from "~/app/eventos-applicacion/eventos-delegate/custom-delegate";
-import {aplicarAuth0Delegate} from "~/app/eventos-applicacion/eventos-delegate/auth0-delegate";
-
+import {aplicarAuth0Delegate} from '@manticore-labs/nativescript';
 
 if (ios) {
     ios.delegate = EventosDelegate;
-    aplicarAuth0Delegate(EventosDelegate);
+    aplicarAuth0Delegate(EventosDelegate)
 }
 
 
