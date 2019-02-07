241ff8d8a965523dab82c43bbde17d5e6b7c6f4c
Adrian Eguez <eadepto@hotmail.com>
2019-02-07 14:58:20 -0500

    arreglar paquetes

--- HelloWorld/src/main.ts
+++ HelloWorld/src/main.ts
@@ -3,7 +3,7 @@ import {platformNativeScriptDynamic} from "nativescript-angular/platform";
 import {AppModule} from "./app/app.module";
 import {ios} from "tns-core-modules/application";
 import {EventosDelegate} from "~/app/eventos-applicacion/eventos-delegate/custom-delegate";
-import {aplicarAuth0Delegate} from '@manticore-labs/nativescript';
+import { aplicarAuth0Delegate } from '@manticore-labs/nativescript';
 
 if (ios) {
     ios.delegate = EventosDelegate;
